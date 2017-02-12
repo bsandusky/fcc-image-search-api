bf73d2e2417855984285e05f170de0023709fc18
Brett <brett.sandusky@gmail.com>
2017-02-12 15:02:31 +0000

    added routes and static index

--- app.js
+++ app.js
@@ -2,9 +2,20 @@
 const express = require("express")
 const app = express()
 
+app.use(express.static("public"))
+
 app.get("/", (req, res) => {
-    res.send("Hello, world")
+    res.render("index.html")
 })
+
+app.get("/api/latest", (req, res) => {
+    res.send("hello, world")
+})
+
+app.get("/api/search", (req, res) => {
+    res.send(req.query.q + " " + req.query.offset)
+})
+
 
 app.listen(process.env.PORT, () => {
     console.log("Server running on " + process.env.IP + ":" + process.env.PORT)
